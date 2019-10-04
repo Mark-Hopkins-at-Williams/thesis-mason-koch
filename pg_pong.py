c42f0beafe99bed2959a06a8e23bfbf4208ad96c
3141592654 <12554811+3141592654@users.noreply.github.com>
2019-10-04 09:45:49 -0700

    Moved calculation of 'gradient' into backpropagation function. Tested by running diff on save.p for new and old versions and finding them to be identical

--- pg_pong.py
+++ pg_pong.py
@@ -1,4 +1,3 @@
- 
 """ Trains an agent with (stochastic) Policy Gradients on Pong. Uses OpenAI Gym.
     Based on http://karpathy.github.io/2016/05/31/rl/."""
 import numpy as np
@@ -30,8 +29,9 @@ rmsprop_cache = { k : np.zeros_like(v) for k,v in model.items() } # rmsprop memo
 def sigmoid(x): 
   return 1.0 / (1.0 + np.exp(-x))
 
-def prepro(I):
-  """ prepro 210x160x3 uint8 frame into 6400 (80x80) 1D float vector """
+def preprocess_pong(I):
+  # There will always be a preprocessing step, but it will look different for different games.
+  # In this case turn 210x160x3 uint8 frame into a 6400 (80x80) 1D float vector.
   I = I[35:195] # crop
   I = I[::2,::2,0] # downsample by factor of 2
   I[I == 144] = 0 # erase background (background type 1)
@@ -61,82 +61,75 @@ def policy_forward(x):
   # Output layer. This is going to look largely the same if we are wanting two probabilities as our output.
   logitp = np.dot(model['W2'], h)
   p = sigmoid(logitp)
-  # Return the number we are interested in (in this case the probability of taking action 2)
-  # and the output of the hidden states
+  # Return the number we are interested in (in this case the probability of taking action 2) and the output
+  # the hidden states. The latter is not strictly necessary, but it will make our lives easier
   return p, h
 
-def policy_backward(hidden_layer_outputs, so_called_gradients):
-  """ backward pass. (hidden_layer_outputs is array of intermediate hidden states) """
-  # If you had a more complicated neural network, you probably would want to call it h instead of hidden_layer_outputs.
-  # But if you had a more complicated neural network, wouldn't you use TensorFlow? It would be much work and much correcting
-  # of typos to do it by hand...
-  dW2 = np.dot(hidden_layer_outputs.T, so_called_gradients).ravel()    # In the base case with one example, this is just
-                                                                       # multiplying the hidden layer outputs by a scalar
-  dhidden_layer = np.outer(so_called_gradients, model['W2'])
-  dhidden_layer[hidden_layer_outputs <= 0] = 0 # backprop relu
-  dW1 = np.dot(dhidden_layer.T, inputs)         # it's not clear how this works given that inputs (the original name was
-  return {'W1':dW1, 'W2':dW2}                   # eps) does not appear to be defined in this scope. Trudge on.
+def policy_backward(xs, hs, prob_action_2s, actions, rewards):
+  # Implement Andrej's vaguely strange gradient
+  actions = (actions - 1) % 2  # 1 if action is 2, 0 otherwise 
+  actions = actions.astype('float64') - prob_action_2s
+
+  discounted_rewards = discount_rewards(rewards)
+  # Standardize the rewards to be unit normal because Andrej says so.
+  discounted_rewards -= np.mean(discounted_rewards)
+  discounted_rewards /= np.std(discounted_rewards)
+  rewards = actions * discounted_rewards
+  # Right, now we have this strange quantity.
+
+  dW2 = (hs.T @ rewards).ravel()
+  dhidden_layer = np.outer(rewards, model['W2'])
+  dhidden_layer[hs <= 0] = 0
+  dW1 = dhidden_layer.T @ xs
+  return {'W1':dW1, 'W2':dW2}
 
 if __name__ == '__main__':
     env = gym.make("Pong-v0")
     observation = env.reset()
     env.seed(42)
     env.action_space.seed(24)
-    prev_x = None # used in computing the difference frame
-    xs,hs,so_called_gradient,drs = [],[],[],[]
+    prev_x = None
+    xs,hs,prob_action_2s,actions,rewards = [],[],[],[],[]
     running_reward = None
     reward_sum = 0
     episode_number = 0
     while True:
       if render: env.render()
     
-      # preprocess the observation, set input to network to be difference image
-      cur_x = prepro(observation)
+      cur_x = preprocess_pong(observation)
       x = cur_x - prev_x if prev_x is not None else np.zeros(D)
       prev_x = cur_x
     
-      # forward the policy network and sample an action from the returned probability
-      aprob, h = policy_forward(x)
-      action = 2 if np.random.uniform() < aprob else 3 # roll the dice!
+      # This neural network outputs the probability of taking action 2. This is unusual, normally it would output
+      # the probabilities for taking each action. But, if we have two actions, it's not wrong.
+      prob_action_2, h = policy_forward(x)
+      action = 2 if np.random.uniform() < prob_action_2 else 3
     
-      # record various intermediates (needed later for backprop)
-      xs.append(x) # observation
-      hs.append(h) # hidden state
-      y = 1 if action == 2 else 0 # a "fake label"
-      so_called_gradient.append(y - aprob) # grad that encourages the action that was taken to be taken (see http://cs231n.github.io/neural-networks-2/#losses if confused)
-      # It isn't really a gradient because it does not satisfy the definition given at https://en.wikipedia.org/wiki/Gradient.
-      # Moreover you need to take the gradient with respect to something, and that doesn't happen for a while yet.
-      # But once we do that, we will be treating it like a gradient. Hence "so_called_gradient".
-    
+      xs.append(x)
+      hs.append(h)    # We don't strictly need to remember this, but it will make our lives easier
+      prob_action_2s.append(prob_action_2)    # Same
+      actions.append(action)
+
       # step the environment and get new measurements
       observation, reward, done, info = env.step(action)
       reward_sum += reward
     
-      drs.append(reward) # record reward (has to be done after we call step() to get reward for previous action)
+      rewards.append(reward)    # Recall that we must see the outcome of the action before we write down the reward for taking it
     
       if done: # an episode finished
         episode_number += 1
     
-        # stack together all inputs, hidden states, action gradients, and rewards for this episode
-        inputs = np.vstack(xs)
-        hidden_layer_outputs = np.vstack(hs)                   # Note that as h is strictly a function of x, we don't actually 
-        so_called_gradients = np.vstack(so_called_gradient)    # need to save these. But it will sure be less work if we do.
-        epr = np.vstack(drs)
-        xs,hs,so_called_gradient,drs = [],[],[],[] # reset array memory
+        # stack together all inputs, hidden states, probabilities, actions and rewards for this episode
+        xs = np.vstack(xs)
+        hs = np.vstack(hs)                # Both h and prob_action_2 are strictly functions of x, so we don't need to
+        prob_action_2s = np.vstack(prob_action_2s)    # remember them. But we should, because that will be less work.
+        actions = np.vstack(actions)
+        rewards = np.vstack(rewards)
     
-        # compute the discounted reward backwards through time
-        discounted_rewards = discount_rewards(epr)
-        # standardize the rewards to be unit normal (helps control the gradient estimator variance)
-        discounted_rewards -= np.mean(discounted_rewards)
-        discounted_rewards /= np.std(discounted_rewards)
-    
-        so_called_gradients *= discounted_rewards # modulate the gradient with advantage (PG magic happens right here.)
-                                                  # Magic is what happens in Harry Potter. 
-        # The particular "gradient" chosen means that the changes in the weights are bigger if we pick the action that 
-        # was less likely, and also bigger if hidden layer outputs are bigger.
-    
-        grad = policy_backward(hidden_layer_outputs, so_called_gradients)
+        # Give backprop everything it could conceivably need
+        grad = policy_backward(xs, hs, prob_action_2s, actions, rewards)
         for k in model: grad_buffer[k] += grad[k] # accumulate grad over batch
+        xs,hs,prob_action_2s,actions,rewards = [],[],[],[],[]
     
         # perform rmsprop parameter update every batch_size episodes
         if episode_number % batch_size == 0:
@@ -162,8 +155,5 @@ if __name__ == '__main__':
           print(('ep %d: game finished, reward: %f' % (episode_number, reward)) + ('' if reward == -1 else ' !!!!!!!!'))
 else:
     print(__name__)
-
-
-
 
 
