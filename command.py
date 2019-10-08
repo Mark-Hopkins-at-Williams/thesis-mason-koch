ce4c7cac32c8523b67af535822fdf54ad07fd92f
3141592654 <12554811+3141592654@users.noreply.github.com>
2019-10-08 16:29:10 -0700

    Modifying example code to run Pokemon was surprisingly painless

--- command.py
+++ command.py
@@ -1,17 +1,17 @@
 import asyncio
 
 async def run(cmd):
-    # Call "Pokemon simulator"
+    # Call Pokemon simulator
     proc = await asyncio.create_subprocess_shell(cmd, stdout=asyncio.subprocess.PIPE)
-    # Sit around waiting for input and exit when the subprocess exits
+    # Sit around waiting for input, exit when the subprocess exits
     data = "."
     while data != "":
         data = await proc.stdout.readline()
         data = data.decode('ascii').rstrip()
-        # Print the data to the screen, because why not?
+        # Print the data to the screen so the user can see it
         print(data)
     await proc.wait()
 
-# In this case, we are running a simple program which returns what we fed it plus a little more
-asyncio.run(run('node hello.js'))
+# Run the random player
+asyncio.run(run('node ./Pokemon-Showdown/.sim-dist/examples/test_random_player.js'))
 
