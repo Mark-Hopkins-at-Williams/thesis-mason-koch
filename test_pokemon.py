d7184674a87861a336938fdd22d6308a501647e2
3141592654 <12554811+3141592654@users.noreply.github.com>
2019-11-09 09:50:18 -0800

    Added tests for preprocess_observation

--- test_pokemon.py
+++ test_pokemon.py
@@ -3,6 +3,7 @@ import numpy as np
 
 import pkmn
 import pickle
+from preprocess_observation import preprocess_observation as preprocess_observation
 
 class TestPokemon(unittest.TestCase):
     
@@ -33,6 +34,25 @@ class TestPokemon(unittest.TestCase):
         action = pkmn.choose_action(x, bookkeeper)
         # Judgement and shadow claw are garbage; so is switching
         assert(action == "move 1" or action == "move 3")
+    
+    def test_preprocessing(self):
+        # First test: give it a plausible start state. Note that the format of the input to preprocess_observation
+        # is not finalised. If I can get the simulator to return a JSON object of the state, we might be looking
+        # at two preprocess_observation classes; one for fighting people online (where you can't just muck around
+        # in the guts of the game and get it to return what you want) and one for training.
+        state_changes, indices = preprocess_observation("|player|p1|Alice||\r\n|player|p2|Bob||\r\n|teamsize|p1|6\r\n|teamsize|p2|6\r\n|gametype|singles\r\n|gen|7\r\n|tier|[Gen 7] Custom Game\r\n|clearpoke\r\n|poke|p1|Ledian, F|item\r\n|poke|p1|Swellow, L10, F|item\r\n|poke|p1|Malamar, L10, M|item\r\n|poke|p1|Houndoom, L10, M|item\r\n|poke|p1|Victreebel, L10, F|item\r\n|poke|p1|Lugia, L10|item\r\n|poke|p2|Arceus-*|item\r\n|poke|p2|Aggron, L10, M|item\r\n|poke|p2|Dragonite, L10, F|item\r\n|poke|p2|Uxie, L10|item\r\n|poke|p2|Cacturne, L10, F|item\r\n|poke|p2|Druddigon, L10, F|item\r\n|teampreview|24\r\n|\r\n|start\r\n|switch|p1a: Ledian|Ledian, F|272/272\r\n|switch|p2a: Arceus|Arceus-Fighting|402/402\r\n|turn|1\r\np2: Arceus\r\np2: Aggron\r\np2: Dragonite\r\np2: Uxie\r\np2: Cacturne\r\np2: Druddigon\r\nDEADBEEF")
+        assert(np.all(indices == [2, 1, 5,3,6,4])) # The order is Arceus/Aggron/Dragonite/Uxie/Cacturne/Druddigon.So Aggron is in position 2, Arceus is in position 1, Cacturne is in position 5, Dragonite is in position 3, Druddigon is in position 6, Uxie is in position 4.
+        assert([0,1] in state_changes)  # Our opponent has Ledian, which is Pokemon 1, on the field
+        assert([1,1] in state_changes)  # Our AI has Arceus on the field, which is coincidentally also Pokemon 1
+        assert([3,272] in state_changes)  # Ledian, which is index 3 overall, has 272 health
+        assert([9,402] in state_changes)  # Arceus, which is position 9 overall, has 402 health
+        # Second test: Give it another plausible start state.
+        state_changes, indices = preprocess_observation("|player|p1|Alice||\r\n|player|p2|Bob||\r\n|teamsize|p1|6\r\n|teamsize|p2|6\r\n|gametype|singles\r\n|gen|7\r\n|tier|[Gen 7] Custom Game\r\n|clearpoke\r\n|poke|p1|Ledian, F|item\r\n|poke|p1|Swellow, L10, F|item\r\n|poke|p1|Malamar, L10, M|item\r\n|poke|p1|Houndoom, L10, M|item\r\n|poke|p1|Victreebel, L10, F|item\r\n|poke|p1|Lugia, L10|item\r\n|poke|p2|Arceus-*|item\r\n|poke|p2|Aggron, L10, M|item\r\n|poke|p2|Dragonite, L10, F|item\r\n|poke|p2|Uxie, L10|item\r\n|poke|p2|Cacturne, L10, F|item\r\n|poke|p2|Druddigon, L10, F|item\r\n|teampreview|24\r\n|\r\n|start\r\n|switch|p1a: Swellow|Ledian, F|42/42\r\n|switch|p2a: Arceus|Arceus-Fighting|402/402\r\n|turn|1\r\np2: Arceus\r\np2: Aggron\r\np2: Dragonite\r\np2: Uxie\r\np2: Cacturne\r\np2: Druddigon\r\nDEADBEEF")
+        assert(np.all(indices == [2, 1, 5,3,6,4]))
+        assert([0,4] in state_changes)  # Now our opponent has Swellow on the field
+        assert([1,1] in state_changes)
+        assert([6,42] in state_changes)
+        assert([9,402] in state_changes)
 
 if __name__ == "__main__":
 	unittest.main()
