bb45bb5a124550bd16dee5377dec865dd3c7921f
3141592654 <12554811+3141592654@users.noreply.github.com>
2020-02-02 12:15:47 -0800

    Modest documentation added

--- game_model.py
+++ game_model.py
@@ -1,21 +1,21 @@
-NUM_POKEMON = 809           # covered in pokedex, which is too big to import here
-TEAM_SIZE = 6
-STATUS_DICT = {'brn': 0, 'par': 1, 'slp': 2, 'frz': 3, 'psn': 4, 'tox': 5, 'confusion': 6}
-NSC_PLACEHOLDER = 7
+NUM_POKEMON = 809           # covered in pokedex, which is too big to import here. a one-hot encoding.
+TEAM_SIZE = 6               # health, measured as a number
+STATUS_DICT = {'brn': 0, 'par': 1, 'slp': 2, 'frz': 3, 'psn': 4, 'tox': 5, 'confusion': 6} 
+NSC_PLACEHOLDER = 7    # status conditions are a one-hot encoding.
 NUM_STATUS_CONDITIONS = 28  # from stream.battle.dataCache.Statuses in battle_stream.
                             # many of them unused because they don't look like status conditions.
                             # also, some of them were introduced in games I haven't played and
                             # am not familiar with. So, this number is not final.
 BOOST_DICT = {'atk': 0, 'def': 1, 'spa': 2, 'spd': 3, 'spe': 4, 'accuracy': 5, 'evasion': 6}
-NUM_STAT_BOOSTS = len(BOOST_DICT)
+NUM_STAT_BOOSTS = len(BOOST_DICT) # three stages of attack boosts is represented as a 3
 WEATHER_DICT = {'': 0, 'raindance': 1, 'primordialsea': 2, 'sunnyday': 3, 'desolateland': 4, 'sandstorm': 5, 'hail': 6, 'deltastream': 7}
-NUM_WEATHER = len(WEATHER_DICT)
+NUM_WEATHER = len(WEATHER_DICT)   # one-hot encoding
 TERRAIN_DICT = {'': 0, 'electricterrain': 1, 'grassyterrain': 2, 'mistyterrain': 3, 'psychicterrain': 4}
 # Sadly, terrain is denoted differently in the game code (above) and in the server messages (below).
 TERRAIN_LOOKUP = {'move: Electric Terrain': 1, 'move: Grassy Terrain': 2, 'move: Misty Terrain': 3, 'move: Psychic Terrain': 4}
-NUM_TERRAIN = len(TERRAIN_DICT)
+NUM_TERRAIN = len(TERRAIN_DICT)   # one-hot encoding
 HAZARD_DICT = {'spikes': 0, 'toxicspikes': 1, 'stealthrock': 2, 'stickyweb': 3}
-NUM_HAZARDS = len(HAZARD_DICT)
+NUM_HAZARDS = len(HAZARD_DICT)    # one-hot encoding
 
 #OUR_TEAM = {'aggron':0, 'arceus':1, 'cacturne':2, 'dragonite':3, 'druddigon':4, 'uxie':5, 0:'aggron', 1:'arceus', 2:'cacturne', 3:'dragonite', 4:'druddigon', 5:'uxie'}
 OUR_TEAM = {'houndoom':0, 'ledian':1, 'lugia':2, 'malamar':3, 'swellow':4, 'victreebel':5, 0:'houndoom', 1:'ledian', 2:'lugia', 3:'malamar', 4:'swellow', 5:'victreebel'}
