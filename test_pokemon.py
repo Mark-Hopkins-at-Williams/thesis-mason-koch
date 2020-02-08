df9dd7b687e4d7bce02183479491cb0325131cd2
3141592654 <12554811+3141592654@users.noreply.github.com>
2020-02-08 11:31:58 -0800

    Test code updated.

--- test_pokemon.py
+++ test_pokemon.py
@@ -4,7 +4,6 @@ import json
 import pkmn
 import pickle
 from preprocess_observation import preprocess_observation
-from preprocess_observation import preprocess_observation_helper
 from bookkeeper import Bookkeeper
 from game_model import *
 
@@ -28,9 +27,9 @@ class TestPokemon(unittest.TestCase):
 
     def test_loaded_model(self):
         f = open('save.p', 'rb')
-        pkmn.model = pickle.load(f)
+        pkmn.list_of_models, _1, _2 = pickle.load(f)
         f.close()
-        bookkeeper = pkmn.Bookkeeper(pkmn.render, pkmn.model, preprocess_observation)
+        bookkeeper = pkmn.Bookkeeper(pkmn.model, preprocess_observation)
         env, observation = pkmn.construct_environment()
         report_observation = bookkeeper.construct_observation_handler()
         x, _ = report_observation(observation)
@@ -56,19 +55,20 @@ class TestPokemon(unittest.TestCase):
               ]},
               "State":
                 ["40/40","272/272","46/46","42/42","37/37","41/41",0, "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", "0", '', '', [], []]}
-        state_changes, indices = preprocess_observation_helper(json.dumps(obs))
-        assert(np.all(indices == [-1, 2, -1,-1,1,-1])) # This depends on what obs you get, and also what team you assume. e.g. this assumes Swellow is second-to-last alphabetically. Which is true at this moment, but it might not be true in a week.
+        state_changes, our_active, opponent_active = preprocess_observation(json.dumps(obs))
         assert([809+305,1] in state_changes)  # Our opponent has Aggron, which is Pokemon 306, on the field
         assert([276,1] in state_changes)  # Our AI has Swellow on the field
         assert([OFFSET_HEALTH + TEAM_SIZE + 0,40] in state_changes)  # Aggron, which is index 0 overall, has 40 health
         assert([OFFSET_HEALTH + 4, 402] in state_changes)  # Swellow's health
+        assert(our_active == 4)
+        assert(opponent_active == 0)
         
     def test_bookkeeper(self):
         f = open('save.p', 'rb')
-        pkmn.model = pickle.load(f)
+        pkmn.model, _1, _2 = pickle.load(f)
         f.close()
         from preprocess_observation import preprocess_observation
-        bookkeeper = Bookkeeper(pkmn.render, pkmn.model, preprocess_observation)
+        bookkeeper = Bookkeeper(pkmn.model, preprocess_observation)
         report_observation = bookkeeper.construct_observation_handler()
         # test report_observation
         report_observation('{"active":[{"moves":[{"move":"Thunderbolt","id":"thunderbolt","pp":24,"maxpp":24,"target":"normal","disabled":false},{"move":"Shadow Claw","id":"shadowclaw","pp":24,"maxpp":24,"target":"normal","disabled":false},{"move":"Aerial Ace","id":"aerialace","pp":32,"maxpp":32,"target":"any","disabled":false},{"move":"Judgment","id":"judgment","pp":16,"maxpp":16,"target":"normal","disabled":false}]}],"side":{"name":"HughMann","id":"p2","pokemon":[{"ident":"p2: Swellow","details":"Swellow","condition":"69/402","active":true,"stats":{"atk":245,"def":297,"spa":297,"spd":297,"spe":297},"moves":["thunderbolt","shadowclaw","aerialace","judgment"],"baseAbility":"guts","item":"flameorb","pokeball":"pokeball","ability":"guts"},{"ident":"p2: Ledian","details":"Ledian, L10, M","condition":"39/39","active":false,"stats":{"atk":32,"def":46,"spa":22,"spd":22,"spe":20},"moves":["roar","heavyslam","rockslide","toxic"],"baseAbility":"swarm","item":"leftovers","pokeball":"pokeball","ability":"sturdy"}]},"State":["40/40","42/272","46/46","42/42","37/37","41/41",0,0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, "", "", [], []]}')
@@ -84,7 +84,7 @@ class TestPokemon(unittest.TestCase):
         assert(np.all(bookkeeper.rewards == [84]))
         assert(len(bookkeeper.rewards) == 1)
         # test report
-        x = np.zeros((n,1), order = 'F')
+        x = np.zeros((N,1), order = 'F')
         x[276] = 1
         x[809+305] = 1
         FULL_HEALTH = 100
@@ -99,22 +99,34 @@ class TestPokemon(unittest.TestCase):
         x[OFFSET_WEATHER] = 1
         x[OFFSET_TERRAIN] = 1
         f = open('save.p', 'rb')
-        pkmn.model = pickle.load(f)
+        pkmn.list_of_models, _1, _2 = pickle.load(f)
         f.close()
-        pvec, h, h2 = pkmn.policy_forward(x, pkmn.model)
+        pvec, h, h2 = pkmn.policy_forward(x, pkmn.list_of_models[bookkeeper.our_active][bookkeeper.opponent_active])
         action = 3
         # Report the same data three times, because why not?
         bookkeeper.report(x, h, h2, pvec, action)
         bookkeeper.report(x, h, h2, pvec, action)
         bookkeeper.report(x, h, h2, pvec, action)
-        xs = np.vstack(bookkeeper.xs).T
-        assert(xs.shape == (n,3))
-        hs = np.vstack(bookkeeper.hs).T
-        assert(hs.shape == (pkmn.H,3))
-        h2s = np.vstack(bookkeeper.h2s).T
-        assert(h2s.shape == (pkmn.H2,3))
-        pvecs = np.vstack(bookkeeper.pvecs).T
-        assert(pvecs.shape == (pkmn.A,3))
+        assert(len(bookkeeper.xs) == 3)
+        assert(bookkeeper.xs[0].shape == (N,1))
+        assert(bookkeeper.xs[1].shape == (N,1))
+        assert(bookkeeper.xs[2].shape == (N,1))
+
+        assert(len(bookkeeper.hs) == 3)
+        assert(bookkeeper.hs[0].shape == (pkmn.H,1))
+        assert(bookkeeper.hs[1].shape == (pkmn.H,1))
+        assert(bookkeeper.hs[2].shape == (pkmn.H,1))
+
+        assert(len(bookkeeper.h2s) == 3)
+        assert(bookkeeper.h2s[0].shape == (pkmn.H2,1))
+        assert(bookkeeper.h2s[1].shape == (pkmn.H2,1))
+        assert(bookkeeper.h2s[2].shape == (pkmn.H2,1))
+
+        assert(len(bookkeeper.pvecs) == 3)
+        assert(bookkeeper.pvecs[0].shape == (pkmn.A,1))
+        assert(bookkeeper.pvecs[1].shape == (pkmn.A,1))
+        assert(bookkeeper.pvecs[2].shape == (pkmn.A,1))
+
         actions = np.vstack(bookkeeper.actions)
         assert(actions.shape == (3,1))
         
